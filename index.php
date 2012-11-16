18c351522e3cc8e7c6e417699f09a202dd69ca58
unknown <mschuessler035@wit-xdvm-048.MILLARDPS.NET>
2012-11-16 11:26:27 -0600

    Added index.php and everything in it

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="icon" type="image/png" href="img/class-icon.jpg" />
+		<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css" />
+		<link rel="stylesheet" type="text/css" href="styles.css" />
+		
+		<title>CLASSES</title>
+	</head>
+	<body>
+		<div class="container">
+			<header>
+				<?php include('layout/header.php'); ?>
+			</header>
+			
+			<nav>
+				<?php include('layout/nav.php'); ?>
+			</nav>
+			
+			<div id="content">
+				<?php include('layout/content.php'); ?>
+			</div>
+			
+			<footer>
+				<?php include('layout/footer.php'); ?>
+			</footer>
+		</div>
+	</body>
+</html>
---
+++ layout/nav.php
@@ -0,0 +1,11 @@
+<div class="navbar">
+	<div class="navbar-inner">
+		<div class="container">
+			<a href="./" class="brand"><img src="img/class-icon.jpg" alt="icon" /></a>
+			<ul class="nav" role="navigation">
+				<li><a href="./?p=classes">Classes</a></li>
+				<li><a href="./?p=form_add_class">Add a Class</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
